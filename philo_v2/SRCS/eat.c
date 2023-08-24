e9d3eb048718e3a7e7e4c9f6682f715d12b475a8
Floriane TUERNAL-SABOTINOV <ftuernal@paul-f4Br3s3.clusters.42paris.fr>
2023-08-24 14:31:02 +0200

    v2 passes all tests

--- philo_v2/SRCS/eat.c
+++ philo_v2/SRCS/eat.c
@@ -6,7 +6,7 @@
 /*   By: ftuernal <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/16 11:46:05 by ftuernal          #+#    #+#             */
-/*   Updated: 2023/08/24 13:35:39 by ftuernal         ###   ########.fr       */
+/*   Updated: 2023/08/24 14:22:51 by ftuernal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ static void	unlock_chopsticks(t_philo *think)
 
 static void	take_chopsticks(t_philo *think)
 {
-	if (think->think_id < think->data->think_nb)
+	if (think->think_id % 2 != 0)
 	{
 		pthread_mutex_lock(think->r_chop);
 		display_status(FORK, think);
@@ -46,10 +46,7 @@ void	eat(t_philo *think)
 		display_status(EAT, think);
 		think->death_end = get_time() + think->data->death_time;
 		if (check_time(get_time() + think->data->eat_time, think->death_end) == -1)
-		{
-printf("coucou\n");
 			go_sleep(think->death_end - get_time());
-		}
 		else
 			go_sleep(think->data->eat_time);
 		think->eat_count += 1;
--- philo_v2/SRCS/get_ataraxia.c
+++ philo_v2/SRCS/get_ataraxia.c
@@ -6,19 +6,40 @@
 /*   By: ftuernal <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/16 11:47:03 by ftuernal          #+#    #+#             */
-/*   Updated: 2023/08/24 13:02:22 by ftuernal         ###   ########.fr       */
+/*   Updated: 2023/08/24 14:26:26 by ftuernal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
+static void	check_thinker(t_philo *think)	
+{
+	pthread_mutex_lock(&think->lock);
+	if (get_time() >= think->death_end && check_death(think) == 0)
+	{
+		write_death(think);
+//		pthread_mutex_unlock(&think->lock);
+//		return (-1);
+	}
+	if (think->data->round_nb > 0 &&
+		think->eat_count >= think->data->round_nb)
+		write_finish(think);
+	pthread_mutex_unlock(&think->lock);
+//	return (0);
+}
+
 void	*death_watch(void *arg)
 {
 	t_philo	*think;
+	int	i;
 
 	think = (t_philo *)arg;
+	usleep(2000);
 	while (1)
 	{
+		i = -1;
+		while (++i < think->data->think_nb)
+			check_thinker(&think->data->think[i]);
 		if (check_death(think) == 1)
 			break ;
 	}
@@ -30,9 +51,14 @@ void	*get_ataraxia(void *arg)
 	t_philo	*think;
 
 	think = (t_philo *)arg;
+	pthread_mutex_lock(&think->lock);
 	think->death_end = think->data->death_time + get_time();
+	pthread_mutex_unlock(&think->lock);
+	if (think->think_id % 2 == 0)
+		go_sleep(think->data->eat_time / 2);
 	while (1)
 	{
+/*
 		pthread_mutex_lock(&think->lock);
 		if (get_time() >= think->death_end && check_death(think) == 0)
 		{
@@ -43,6 +69,7 @@ void	*get_ataraxia(void *arg)
 		if (think->eat_count >= think->data->round_nb - 1)
 			write_finish(think);
 		pthread_mutex_unlock(&think->lock);
+*/
 		if (check_death(think) == 1 || check_finish(think) == 1)
 			break ;
 		eat(think);
