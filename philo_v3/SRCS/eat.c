356b26579217e9e5e5b2c4167a48b87ed37654b4
Floriane TUERNAL-SABOTINOV <ftuernal@paul-f4Br3s3.clusters.42paris.fr>
2023-08-24 12:00:14 +0200

    bash script draft for testing

--- philo_v3/SRCS/eat.c
+++ philo_v3/SRCS/eat.c
@@ -6,37 +6,12 @@
 /*   By: ftuernal <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/16 11:46:05 by ftuernal          #+#    #+#             */
-/*   Updated: 2023/08/24 09:44:59 by ftuernal         ###   ########.fr       */
+/*   Updated: 2023/08/24 11:57:12 by ftuernal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
-/*
-static void	unlock_chopsticks(t_philo *think)
-{
-	pthread_mutex_unlock(think->l_chop);
-	pthread_mutex_unlock(think->r_chop);
-	search_for_truth(think);
-}
 
-static void	take_chopsticks(t_philo *think)
-{
-	if (think->think_id < think->data->think_nb)
-	{
-		pthread_mutex_lock(think->r_chop);
-		display_status(FORK, think);
-		pthread_mutex_lock(think->l_chop);
-		display_status(FORK, think);
-	}
-	else
-	{
-		pthread_mutex_lock(think->l_chop);
-		display_status(FORK, think);
-		pthread_mutex_lock(think->r_chop);
-		display_status(FORK, think);
-	}
-}
-*/
 void	eat(t_philo *think)
 {
 	if (check_death(think) == 0)
@@ -46,12 +21,12 @@ void	eat(t_philo *think)
 		think->death_end = get_time() + think->data->death_time;
 		if (check_time(get_time() + think->data->eat_time, think->death_end) == -1)
 		{
-printf("coucou eat_time\n");
+			//think->death_end = get_time() + think->data->death_time;
 			go_sleep(think->death_end - get_time());
 		}
 		else
 		{
-		//	think->death_end = get_time() + think->data->death_time;
+			//think->death_end = get_time() + think->data->death_time;
 			go_sleep(think->data->eat_time);
 		}
 		think->eat_count += 1;
---
+++ philo_v3/tester.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+
+# quick tester for philosopher
+
+./philo 200 150 60 60 > 200_150_60_60.txt 2>&1
+philo_pid=$!
+
+sleep 20
+
+kill -9 $philo_pid
+if grep -q "died" 200_150_60_60.txt; then
+	echo -e "\033[0;31m200 150 60 60 -- FAILED!\033[0m"
+else
+	echo -e "\033[0;32m200 150 60 60 -- SUCCESS!\033[0m"
+fi
+
+./philo 4 410 200 200 > 4_410_200_200.txt 2>&1
+philo_pid=$!
+
+sleep 20
+
+kill -9 $philo_pid
+if grep -q "died" 4_410_200_200.txt; then
+	echo -e "\033[0;31m4 410 200 200 -- FAILED!\033[0m"
+else
+	echo -e "\033[0;32m4 410 200 200 -- SUCCESS!\033[0m"
+fi
+
+./philo 5 800 200 200 7 > 5_800_200_200_7.txt 2>&1
+philo_pid=$!
+
+sleep 20
+
+kill -9 $philo_pid
+if grep -q "died" 5_800_200_200_7.txt; then
+	echo -e "\033[0;31m200 150 60 60 -- FAILED (diedd)!\033[0m"
+else
+	eating_count=$(grep -o "1 is eating\|2 is eating\|3 is eating\|4 is eating\|5 is eating" 5_800_200_200_7.txt | wc -l)
+	if [ "$eating_count" -lt 7 ]; then
+        echo -e "\033[0;31m5 800 200 200 7 -- FAILED! (Less than 7 eating occurrences)\033[0m"
+    else
+        echo -e "\033[0;32m5 800 200 200 7 -- SUCCESS!\033[0m"
+    fi
+fi
+
+./philo 5 800 200 200 > 5_800_200_200.txt 2>&1
+philo_pid=$!
+
+sleep 20
+
+kill -9 $philo_pid
+if grep -q "died" 5_800_200_200.txt; then
+	echo -e "\033[0;31m5 800 200 200 -- FAILED!\033[0m"
+else
+	echo -e "\033[0;32m5 800 200 200 -- SUCCESS!\033[0m"
+fi
+
+./philo 3 620 200 200 > 3_620_200_200.txt 2>&1
+philo_pid=$!
+
+sleep 20
+
+kill -9 $philo_pid
+if grep -q "died" 3_620_200_200.txt; then
+	echo -e "\033[0;31m3 620 200 200 -- FAILED!\033[0m"
+else
+	echo -e "\033[0;32m3 620 200 200 1 -- SUCCESS!\033[0m"
+fi
+
+./philo 4 310 200 100 > 4_310_200_100.txt 2>&1
+philo_pid=$!
+
+sleep 20
+
+kill -9 $philo_pid
+if grep -q "died" 4_310_200_100.txt; then
+	echo -e "\033[0;32m4 310 200 100 -- SUCCESS!\033[0m"
+else
+	echo -e "\033[0;31m4 310 200 100 -- FAILED!\033[0m"
+fi
+
+./philo 2 410 200 200 > 2_410_200_200.txt 2>&1
+philo_pid=$!
+
+sleep 20
+
+kill -9 $philo_pid
+if grep -q "died" 2_410_200_200.txt; then
+	echo -e "\033[0;31m2 410 200 200 -- FAILED!\033[0m"
+else
+	echo -e "\033[0;32m2 410 200 200 -- SUCCESS!\033[0m"
+fi
+
+mv *.txt TESTS_LOGS/
+
+#4 410 200 200
+#5 800 200 200 7
+#5 800 200 200
+#3 620 200 200
+#4 310 200 100 --> should stop
+#2 410 200 200
